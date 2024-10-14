03a38735a86b7cd8fc5888ee1d313b5f44ddabdd
ojiiis <official.ojingirisamuel@gmail.com>
2024-10-14 13:14:54 +0100

    222

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, View, ActivityIndicator, Image,Dimensions } from 'react-native';
+import { WebView } from 'react-native-webview';
+const { width, height } = Dimensions.get('window');
 export default function App() {
+  const [loading, setLoading] = useState(true); // State to track loading status
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loading && (
+        // Show the loading image or ActivityIndicator while the WebView is loading
+        <View style={styles.loadingContainer}>
+          <Image
+            source={require('./assets/before-load.png')} // Replace with your loading image URL
+            style={{width:width,height:height,resizeMode: 'cover'}}
+            
+          />
+          {/* Or you can use a spinner (ActivityIndicator) */}
+          {/* <ActivityIndicator size="large" color="#0000ff" /> */}
+        </View>
+      )}
+      <WebView
+        source={{ uri: 'http://ojiiis.github.io/ops/' }} // Your website URL
+        onLoadStart={() => setLoading(true)}  // Show loader when loading starts
+        onLoadEnd={() => setLoading(false)}   // Hide loader when loading ends
+        style={{ flex: 1 }}
+      />
     </View>
   );
 }
@@ -13,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  loadingContainer: {
+    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',  // Optional background color while loading
+  },
+ 
 });
