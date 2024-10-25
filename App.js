575a12eb41acd265347748b2a70bb2f6e34ca28b
ojiiis <official.ojingirisamuel@gmail.com>
2024-10-25 14:32:57 +0100

    01

--- App.js
+++ App.js
@@ -1,45 +1,30 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, Dimensions, StatusBar } from 'react-native';
+//import { StyleSheet } from 'react-native';
+import { StyleSheet,SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
-
-const { width, height } = Dimensions.get('window');
-
+//import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 export default function App() {
-  const [loading, setLoading] = useState(true); // State to track loading status
-
+ 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      {loading && (
-        // Show the loading image while the WebView is loading
-        <View style={styles.loadingContainer}>
-          <Image
-            source={require('./assets/before-load.png')} // Replace with your loading image
-            style={{ width: width, height: height, resizeMode: 'cover' }}
-          />
-        </View>
-      )}
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="#f8f8f8" />
+    <SafeAreaView style={styles.container}>
       <WebView
         source={{ uri: 'http://ojiiis.github.io/ops/' }} // Your website URL
-        onLoadStart={() => setLoading(true)}  // Show loader when loading starts
-        onLoadEnd={() => setLoading(false)}   // Hide loader when loading ends
-        style={styles.webView}
+      //  onLoadStart={() => setLoading(true)}  // Show loader when loading starts
+     // onLoadEnd={() => setLoading(false)}   // Hide loader when loading ends
+        style={{ flex: 1 }}
       />
-    </View>
+    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  loadingContainer: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'center',
+     backgroundColor: '#f8f8f8',
     alignItems: 'center',
-    backgroundColor: 'white',  // Optional background color while loading
-  },
-  webView: {
-    flex: 1,
-  },
+    justifyContent: 'center',
+  }
+ 
 });
