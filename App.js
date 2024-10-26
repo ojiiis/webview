d806bf7a65a1d0f4fc18cce53973de7c78dc8d1f
Ojingiri Samuel <89736223+ojiiis@users.noreply.github.com>
2024-10-26 12:06:58 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-//import { StyleSheet } from 'react-native';
-import { StyleSheet,SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
-//import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 export default function App() {
- 
   return (
     <>
-    <StatusBar barStyle="dark-content" backgroundColor="#f8f8f8" />
-    <SafeAreaView style={styles.container}>
-      <WebView
-        source={{ uri: 'http://ojiiis.github.io/ops/' }} // Your website URL
-      //  onLoadStart={() => setLoading(true)}  // Show loader when loading starts
-     // onLoadEnd={() => setLoading(false)}   // Hide loader when loading ends
-        style={{ flex: 1 }}
-      />
-    </SafeAreaView>
+      <StatusBar barStyle="dark-content" backgroundColor="#f8f8f8" />
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: 'http://ojiiis.github.io/ops/' }} // Your website URL
+          style={{ flex: 1 }}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -22,9 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-     backgroundColor: '#f8f8f8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
- 
+    backgroundColor: '#f8f8f8',
+  },
 });
